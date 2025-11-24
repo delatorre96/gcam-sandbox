c4dd8ca61f091eeb7c09405dff386a5acc3245e3
Ignacio de la Torre <idelatorrecubillo@gmail.com>
2025-11-24 09:48:16 +0100

    out xmlextraction

--- get_xmlData/output/setup.py
+++
@@ -1,8 +0,0 @@
-# setup.py
-from setuptools import setup
-from Cython.Build import cythonize
-import cython
-
-setup(
-    ext_modules = cythonize("timeseries_cy.pyx", compiler_directives={'language_level' : "3"})
-)
